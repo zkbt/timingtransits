b7f40d071c7f142e0fdd06cb37502ff7f831ae9a
Zach Berta-Thompson <zkbt@mit.edu>
2017-01-25 14:57:49 -0700

    add some snark

--- simulate.py
+++ simulate.py
@@ -2,6 +2,7 @@ import numpy as np
 import matplotlib.pyplot as plt
 
 def simulate(cadence=2.0/60.0/24.0, noise=0.001):
+    # gosh, I should add a doc string!
     time = np.arange(-0.25, 0.25, cadence)
     flux = np.ones_like(time)
     noisy = noise*np.random.normal(0, 1, len(flux)) + flux
