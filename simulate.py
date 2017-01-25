413d5dc181fe515fdc289bd4ba0795aa574fdcd8
Zach Berta-Thompson <zkbt@mit.edu>
2017-01-25 14:41:56 -0700

    add simulation code

--- simulate.py
+++ simulate.py
@@ -3,4 +3,6 @@ import matplotlib.pyplot as plt
 
 def simulate(cadence=2.0/60.0/24.0, noise=0.001):
     time = np.arange(-0.25, 0.25, cadence)
-    
+    flux = np.ones_like(time)
+    noisy = noise*np.random.normal(0, 1, len(flux)) + flux
+    return time, noisy
